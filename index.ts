aada9d5c75dae70483570a6e3582e1ec682d52ce
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-05-04 22:26:26 +0700

    feat: fetch all users and sort by followers

--- index.ts
+++ index.ts
@@ -10,7 +10,9 @@ const REPO = process.env.GITHUB_REPO;
 const CACHE_FOLDER = '.cache';
 const CURRENT_PAGE_CACHE = path.join(CACHE_FOLDER, 'currentPage.txt');
 
-const OUTPUT = 'output.json';
+const OUTPUT_STARGAZERS = 'output_stagazers.json';
+const OUTPUT_DETAILED_USERS = 'output_detailed_users.json';
+const OUTPUT_FOLLOWERS = 'output_followers.json';
 
 if (!TOKEN || !OWNER || !REPO) {
   throw new Error(
@@ -48,6 +50,41 @@ interface Stagazer {
   site_admin: boolean;
 }
 
+interface User {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: false;
+  name: string;
+  company: string;
+  blog: string;
+  location: null;
+  email: null;
+  hireable: true;
+  bio: string;
+  twitter_username: string;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: string;
+  updated_at: string;
+}
+
 interface StarListResult {
   hasNextPage: boolean;
   stargazers: any[];
@@ -55,16 +92,16 @@ interface StarListResult {
 
 function appendDataToFile(stargazers: Stagazer[]) {
   let data = [] as Stagazer[];
-  if (fs.existsSync(OUTPUT)) {
+  if (fs.existsSync(OUTPUT_STARGAZERS)) {
     try {
-      data = JSON.parse(fs.readFileSync(OUTPUT, 'utf8'));
+      data = JSON.parse(fs.readFileSync(OUTPUT_STARGAZERS, 'utf8'));
     } catch (error) {
       console.error(error);
     }
   }
 
   const newData = [...data, ...stargazers];
-  fs.writeFileSync(OUTPUT, JSON.stringify(newData));
+  fs.writeFileSync(OUTPUT_STARGAZERS, JSON.stringify(newData));
 }
 
 async function fetchPage(page: number): Promise<StarListResult> {
@@ -92,6 +129,14 @@ function createFolderIfNotExisted(folder: string) {
   }
 }
 
+async function getUserInfo(username: string) {
+  const response = await octokit.request<User>({
+    method: 'get',
+    url: `/users/${username}`,
+  });
+  return response.data;
+}
+
 async function main() {
   // Read current page since last time (start to crawl from currentPage-1)
   createFolderIfNotExisted(CACHE_FOLDER);
@@ -103,8 +148,8 @@ async function main() {
   //   currentPage = Number(fs.readFileSync(CURRENT_PAGE_CACHE, 'utf8'));
   // }
   // Just remove the output file if existed first
-  if (fs.existsSync(OUTPUT)) {
-    fs.unlinkSync(OUTPUT);
+  if (fs.existsSync(OUTPUT_STARGAZERS)) {
+    fs.unlinkSync(OUTPUT_STARGAZERS);
   }
 
   let haveNextPage = true;
@@ -116,8 +161,59 @@ async function main() {
   }
 
   // Count number of followers (Checksum)
-  const stargazers = JSON.parse(fs.readFileSync(OUTPUT, 'utf8'));
+  const stargazers = JSON.parse(
+    fs.readFileSync(OUTPUT_STARGAZERS, 'utf8'),
+  ) as Stagazer[];
   console.log('Total stargazers', stargazers.length);
 }
 
-main();
+async function fetchAllUsers() {
+  if (!fs.existsSync(OUTPUT_STARGAZERS)) {
+    throw new Error('Stagazers file does not exist. Fetch stagazers first.');
+  }
+
+  const stargazers = JSON.parse(
+    fs.readFileSync(OUTPUT_STARGAZERS, 'utf8'),
+  ) as Stagazer[];
+
+  // Fetch all user info and save to disk
+  const detailedStargazers = await Promise.all(
+    stargazers.map(async (stargazer) => {
+      console.log(`Fetching user info for ${stargazer.login}`);
+      const userInfo = await getUserInfo(stargazer.login);
+      return userInfo;
+    }),
+  );
+  console.log('detailedStargazers', detailedStargazers);
+  fs.writeFileSync(OUTPUT_DETAILED_USERS, JSON.stringify(detailedStargazers));
+}
+
+function reportFollowerInfo() {
+  const users = JSON.parse(
+    fs.readFileSync(OUTPUT_DETAILED_USERS, 'utf8'),
+  ) as User[];
+
+  let userWithFollowers = users.map((user) => ({
+    login: user.login,
+    followers: user.followers,
+    company: user.company,
+    location: user.location,
+  }));
+
+  userWithFollowers.sort((a, b) => b.followers - a.followers);
+  userWithFollowers = userWithFollowers.map((user, index) => ({
+    rank: index + 1,
+    ...user,
+  }));
+  fs.writeFileSync(
+    OUTPUT_FOLLOWERS,
+    JSON.stringify(userWithFollowers, null, 2),
+  );
+}
+
+function main2() {
+  reportFollowerInfo();
+}
+
+main2();
+// main();
